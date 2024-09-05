7c0eed97235fc09a2889afc79850b57508e82fe4
NikolaNikolikj <Nikola_nikolik@hotmail.com>
2024-09-05 20:39:43 +0200

    - add model for User - edit the db test to use the User model

--- Tests/posgress.test.ts
+++ Tests/posgress.test.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import {PostgresClient} from "../database/PostgresClient";
+import {User} from "../model/database/User";
 
 dotenv.config();
 
@@ -11,13 +12,16 @@ describe('PostgresClient', () => {
     });
 
     it('should execute the SELECT query and return results', async () => {
-        const query = `SELECT x.* FROM rnacen.auth_user x WHERE id = '1'`;
-            const result = await dbClient.query(query);
-            expect(result.rows).toBeDefined();
-            expect(result.rows.length).toBeGreaterThan(0); // Adjust as needed based on expected results
-            // @ts-ignore
-        expect(result.rows[0].password).toEqual('pbkdf2_sha256$390000$cJyweYFz9OhKkF5WeO7ZdJ$ylpeSAfTQdBw4dH1nriuyp1db/gX4Bpqcqh4gw7MS7U=');
+        const userId = '1';
+        const result = await dbClient.query<User>(`SELECT * FROM rnacen.auth_user WHERE id = $1`, [userId]);
 
+        expect(result.rows).toBeDefined();
+        expect(result.rows.length).toBeGreaterThan(0);
+
+        const user = result.rows[0];
+        expect(user.id).toEqual(1);
+        expect(user.username).toEqual('ribas');
+        expect(user.password).toEqual('pbkdf2_sha256$390000$cJyweYFz9OhKkF5WeO7ZdJ$ylpeSAfTQdBw4dH1nriuyp1db/gX4Bpqcqh4gw7MS7U=');
     });
 
     afterAll(async () => {
